20454a9e040eaab66ef0af14b8cdfb86ce0f4bf9
mrgann <mrgann@Debian.myguest.virtualbox.org>
2024-01-08 22:13:11 -0300

    Improve setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
-# Loads the configuration files into the home directory.
-# WARNING: THIS HAS NOT BEEN TESTED YET, USE AT YOUR OWN RISK!
+#!/usr/bin/env bash
 
+# Loads the configuration files into the home directory.
+# WARNING: This will copy and replace files in your home directory
+
+# Get the script directory
+parent_path=$(
+	cd "$(dirname "${BASH_SOURCE[0]}")"
+	pwd -P
+)
+cd "$parent_path"
+echo $(pwd)
 # Copy forced and recursively
-cp -rf home/* ~/
+cp -rvfi ../home/ ~/
